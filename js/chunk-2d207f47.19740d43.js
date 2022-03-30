eacf5da1a37eefb6d241c29674381a1a38330ffb
WebDebz <47834764+WebDebz@users.noreply.github.com>
2022-03-30 11:35:16 -0400

    Add files via upload

---
+++ js/chunk-2d207f47.19740d43.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d207f47"],{a376:function(o){o.exports=JSON.parse('{"english":"English","spanish":"Española","language":"Española","home":"Hogar","listData":"Datos de lista","lastModified":"Última modificación","prospect":"Perspectiva","status":"Estado","locked":"¿Bloqueado?","incomplete":"Incompleto","complete":"Completo","submitted":"Presentado","duplicate":"Duplicado","unknown":"Desconocido"}')}}]);
+//# sourceMappingURL=chunk-2d207f47.19740d43.js.map
